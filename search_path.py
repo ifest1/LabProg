f693303c431db2475cb85d0c55c106afd7e89732
iagofilipe <38733875+instructionpointer@users.noreply.github.com>
2019-09-13 15:50:00 -0300

    exercício Linearlândia por busca em largura.

---
+++ search_path.py
@@ -0,0 +1,47 @@
+import queue
+vertex, origin, destiny = [int(n) for n in input().split()]
+graph = {new_list: [] for new_list in range(1, vertex+1)}
+edges = []
+print(graph)
+for i in range(vertex-1):
+    v1, v2, weight = [int(n) for n in input().split()]
+    edges.append((v1, v2, weight))
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+
+def BFS():
+    q = queue.Queue()
+    visiteds = [origin]
+    q.put(origin)
+    flag = True
+    stack = []
+
+    while not q.empty():
+        if flag:
+            v = q.get()
+            stack.append(v)
+        
+        print(stack)
+
+        if v == destiny:
+            return stack
+
+        if v not in graph:
+            flag = False
+            v = stack.pop()
+            continue
+
+        else:
+            flag = True
+
+        for w in graph[v]:
+            if w not in visiteds:
+                visiteds.append(w)
+                q.put(w)
+
+
+
+print(graph)
+#print(edges)
+print(BFS())
